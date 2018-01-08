6966d5674ff4f1f0ecb4e3e136f3bab26535c33b
Giucu <giucu91@yahoo.com>
2018-01-08 13:03:14 +0200

    Fixed tracking URL. Removed documentation button

--- inc/admin/support.php
+++ inc/admin/support.php
@@ -4,25 +4,14 @@ if ( ! defined( 'WPINC' ) ) {
 }
 ?>
 <div class="feature-section sab-support">
-	<div class="row two-col">
+	<div class="row two-col center-support">
 		<div class="col">
 			<h3><i class="dashicons dashicons-sos" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Contact Support', 'saboxplugin' ); ?></h3>
 			<p>
 				<i><?php esc_html_e( 'We offer excellent support through our advanced ticketing system. Make sure to register your purchase before contacting support!', 'saboxplugin' ); ?></i>
 			</p>
 			<p><a target="_blank" class="button button-primary"
-				  href="<?php echo esc_url( 'https://www.machothemes.com/support/?utm_source=sab&utm_medium=customizer' ); ?>"><?php esc_html_e( 'Contact Support', 'saboxplugin' ); ?></a>
-			</p>
-		</div><!--/.col-->
-
-		<div class="col">
-			<h3><i class="dashicons dashicons-book-alt" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Documentation', 'saboxplugin' ); ?></h3>
-			<p>
-				<i><?php esc_html_e( 'This is the place to go to reference different aspects of the theme. Our online documentation is an incredible resource for learning the ins and outs of using our theme.', 'saboxplugin' ); ?></i>
-			</p>
-			<p>
-				<a target="_blank"
-				   href="<?php echo esc_url( 'http://docs.machothemes.com/collection/123-medzone' ); ?>"><?php esc_html_e( 'See our full documentation', 'saboxplugin' ); ?></a>
+				  href="<?php echo esc_url( 'https://www.machothemes.com/support/?utm_source=sab&utm_medium=about-page&utm_campaign=support-button' ); ?>"><?php esc_html_e( 'Contact Support', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 	</div>
@@ -33,7 +22,7 @@ if ( ! defined( 'WPINC' ) ) {
 				<i><?php esc_html_e( 'Take a look at why website speed is crucial and ways you can speed up WordPress so that you can confidently say you hold your own amongst the billions of web pages!', 'saboxplugin' ); ?></i>
 			</p>
 			<p><a target="_blank"
-				  href="<?php echo esc_url( 'https://www.machothemes.com/blog/how-to-speed-up-wordpress/?utm_source=worg&utm_medium=customizer&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
+				  href="<?php echo esc_url( 'https://www.machothemes.com/blog/how-to-speed-up-wordpress/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 
@@ -44,7 +33,7 @@ if ( ! defined( 'WPINC' ) ) {
 			</p>
 			<p>
 				<a target="_blank"
-				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/best-wordpress-security-plugins/?utm_source=worg&utm_medium=customizer&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
+				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/best-wordpress-security-plugins/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 
@@ -55,7 +44,7 @@ if ( ! defined( 'WPINC' ) ) {
 			</p>
 			<p>
 				<a target="_blank"
-				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/cheap-wordpress-hosting/?utm_source=worg&utm_medium=customizer&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
+				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/cheap-wordpress-hosting/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 	</div>
