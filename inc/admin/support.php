b44949887afca65a21e5e111dda5e8f9b0435268
Giucu <giucu91@yahoo.com>
2018-01-09 12:35:46 +0200

    Changed Support Page

--- inc/admin/support.php
+++ inc/admin/support.php
@@ -8,43 +8,46 @@ if ( ! defined( 'WPINC' ) ) {
 		<div class="col">
 			<h3><i class="dashicons dashicons-sos" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Contact Support', 'saboxplugin' ); ?></h3>
 			<p>
-				<i><?php esc_html_e( 'We offer excellent support through our advanced ticketing system. Make sure to register your purchase before contacting support!', 'saboxplugin' ); ?></i>
+				<i><?php esc_html_e( 'We offer excellent support through our advanced ticketing system.', 'saboxplugin' ); ?></i>
 			</p>
-			<p><a target="_blank" class="button button-primary"
+			<p><a target="_blank" class="button button-hero button-primary"
 				  href="<?php echo esc_url( 'https://www.machothemes.com/support/?utm_source=sab&utm_medium=about-page&utm_campaign=support-button' ); ?>"><?php esc_html_e( 'Contact Support', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 	</div>
-	<div class="row three-col">
+	<div class="row">
+		<h1 class="sab-title">Looking for better WP hosting ?</h1>
+	</div>
+	<div class="row sab-blog three-col">
 		<div class="col">
-			<h3><i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'How to speed up WordPress', 'saboxplugin' ); ?></h3>
+			<h3><i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Our Bluehost Hosting Review', 'saboxplugin' ); ?></h3>
 			<p>
-				<i><?php esc_html_e( 'Take a look at why website speed is crucial and ways you can speed up WordPress so that you can confidently say you hold your own amongst the billions of web pages!', 'saboxplugin' ); ?></i>
+				<i><?php esc_html_e( 'Despite its popularity, though, Bluehost often carries a negative perception among WordPress professionals. So as we dig into this Bluehost review, we\'ll be looking to figure out whether Bluehost\'s performance and features actually justify that reputation.', 'saboxplugin' ); ?></i>
 			</p>
 			<p><a target="_blank"
-				  href="<?php echo esc_url( 'https://www.machothemes.com/blog/how-to-speed-up-wordpress/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
+				  href="<?php echo esc_url( 'https://www.machothemes.com/blog/bluehost-review/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 
 		<div class="col">
-			<h3><i class="dashicons dashicons-shield" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Best security plugins', 'saboxplugin' ); ?></h3>
+			<h3><i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Our InMotion Hosting Review', 'saboxplugin' ); ?></h3>
 			<p>
-				<i><?php esc_html_e( 'Thinking that the best WordPress security plugins aren’t going to come cheap, not to mention free? Luckily, that’s not the case.', 'saboxplugin' ); ?></i>
+				<i><?php esc_html_e( 'InMotion Hosting is a popular independent web host that serves over 300,000 customers. They\'re notably not a part of the EIG behemoth (the parent company behind Bluehost, HostGator, and more), which is a plus in my book.', 'saboxplugin' ); ?></i>
 			</p>
 			<p>
 				<a target="_blank"
-				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/best-wordpress-security-plugins/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
+				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/inmotion-hosting-review/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 
 		<div class="col">
-			<h3><i class="dashicons dashicons-analytics" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Cheap WordPress hosting', 'saboxplugin' ); ?></h3>
+			<h3><i class="dashicons dashicons-performance" style="display: inline-block;vertical-align: middle;margin-right: 5px"></i><?php esc_html_e( 'Our A2 Hosting Review', 'saboxplugin' ); ?></h3>
 			<p>
-				<i><?php esc_html_e( 'There are so many services offering cheap WordPress hosting out there that picking a winner can often seem like a game of chance. This article will hopefully allow you to make an informed decision, catered to your needs', 'saboxplugin' ); ?></i>
+				<i><?php esc_html_e( 'When it comes to affordable WordPress hosting, A2 Hosting is a name that often comes up in various WordPress groups for offering quick-loading performance that belies its low price tag.', 'saboxplugin' ); ?></i>
 			</p>
 			<p>
 				<a target="_blank"
-				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/cheap-wordpress-hosting/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
+				   href="<?php echo esc_url( 'https://www.machothemes.com/blog/a2-hosting-review/?utm_source=sab&utm_medium=about-page&utm_campaign=blog-links' ); ?>"><?php esc_html_e( 'Read more', 'saboxplugin' ); ?></a>
 			</p>
 		</div><!--/.col-->
 	</div>
