68ed660e51f0feae0d0cfb0fb75e90913cc98bf5
cristianraiber <cristian@machothemes.com>
2018-04-19 17:27:51 +0300

    sabox-admin.js rollback

--- assets/js/sabox-admin.js
+++ assets/js/sabox-admin.js
@@ -3,8 +3,6 @@
 
 	'use strict';
 	var context = $( '#sabox-container' );
-
-
 	context.find( '.saboxfield' ).on( 'change', function() {
 
 		var value = getElementValue( $( this ) );
@@ -32,9 +30,6 @@
 			return $element.val();
 		}
 	}
-
-
-
 
 	/**
 	 * Handle UI tab switching via jQuery instead of relying on CSS only
@@ -90,7 +85,16 @@
 			sliders = context.find( '.sabox-slider' ),
 			colorpickers = context.find( '.sabox-color' );
 
-
+		elements.each( function( $index, $element ) {
+			var element = $( $element ),
+				value = getElementValue( element ),
+				elements = context.find( '.show_if_' + element.attr( 'id' ) );
+			if ( value && '0' !== value ) {
+				elements.removeClass( 'hide' );
+			} else {
+				elements.addClass( 'hide' );
+			}
+		} );
 		if ( sliders.length > 0 ) {
 			sliders.each( function( $index, $slider ) {
 				var input = $( $slider ).parent().find( '.saboxfield' ),
@@ -120,5 +124,8 @@
 
 	} );
 
+	$( document ).change( function() {
+
+	} );
 
 })( jQuery );
